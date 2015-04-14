05a31679c530219b31348b9be1b57590c925a081
Łukasz Biedrycki <lukasz.biedrycki@gmail.com>
2015-04-14 14:50:29 +0100

    Add rafter_leader_election test using proper

---
+++ test/rafter_leader_election_proper.erl
@@ -0,0 +1,77 @@
+-module(rafter_leader_election_proper).
+
+-ifdef(PROPER).
+
+-include_lib("proper/include/proper.hrl").
+-include_lib("eunit/include/eunit.hrl").
+
+-include("rafter.hrl").
+
+-compile(export_all).
+
+%% ====================================================================
+%% Tests
+%% ====================================================================
+
+proper_test_() ->
+    {spawn,
+     [
+      {setup,
+       fun setup/0,
+       fun cleanup/1,
+       [%% Run the quickcheck tests
+        {timeout, 30,
+         ?_assertEqual(true,
+                       proper:quickcheck(
+                         proper:numtests(100, prop_leader_elected())))}
+       ]
+      }
+     ]
+    }.
+
+setup() ->
+    ok.
+
+cleanup(_) ->
+    ok.
+
+%% ====================================================================
+%% Properties
+%% ====================================================================
+
+prop_leader_elected() ->
+    rafter:start_cluster(),
+    assert_each_node_is_follower_with_blank_config(),
+    rafter:set_config(peer1, peers()),
+    %% leader election should occur in less than 1 second with the defaults.
+    %% This is a liveness constraint I'd like to maintain if possible.
+    timer:sleep(1000),
+    assert_exactly_one_leader(),
+    application:stop(rafter),
+    true.
+
+assert_each_node_is_follower_with_blank_config() ->
+    Status = get_status_from_peers(),
+    Followers = lists:filter(fun(PeerStatus) ->
+                              {status, _, _, [_, _, _, _, List]} = PeerStatus,
+                              {data, Proplist} = lists:nth(2, List),
+                              follower =:= proplists:get_value("StateName", Proplist)
+                           end, Status),
+   ?assertEqual(5, length(Followers)).
+
+assert_exactly_one_leader() ->
+    Status = get_status_from_peers(),
+    Leaders = lists:filter(fun(PeerStatus) ->
+                              {status, _, _, [_, _, _, _, List]} = PeerStatus,
+                              {data, Proplist} = lists:nth(2, List),
+                              leader =:= proplists:get_value("StateName", Proplist)
+                           end, Status),
+   ?assertEqual(1, length(Leaders)).
+
+peers() ->
+    [peer1, peer2, peer3, peer4, peer5].
+
+get_status_from_peers() ->
+    [sys:get_status(Peer) || Peer <- peers()].
+
+-endif.
